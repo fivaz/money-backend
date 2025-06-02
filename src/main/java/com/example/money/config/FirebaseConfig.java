c5f744fc7c901ec082b3cbfcd10f162ee6f7d6aa
fivaz <s.a.fivaz@gmail.com>
2025-06-02 22:57:38 +0200

    change way to store environment variable

--- src/main/java/com/example/money/config/FirebaseConfig.java
+++ src/main/java/com/example/money/config/FirebaseConfig.java
@@ -3,19 +3,25 @@ package com.example.money.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
 
-    @PostConstruct
-    public void init() throws IOException {
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(getClass().getClassLoader().getResourceAsStream("firebase-credentials.json"))).build();
+    @Value("${firebase.credentials.json}")
+    private String firebaseCredentialsJson;
 
+    @PostConstruct
+    public void initialize() throws IOException {
+        ByteArrayInputStream credentialsStream = new ByteArrayInputStream(firebaseCredentialsJson.getBytes());
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(credentialsStream))
+                .build();
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
         }
