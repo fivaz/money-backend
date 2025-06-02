752d5cd2875c17eb190dc1fd0a515ac60787a9f3
fivaz <s.a.fivaz@gmail.com>
2025-06-02 19:50:38 +0200

    remove db test

--- src/test/java/com/example/money/MoneyApplicationTests.java
+++ src/test/java/com/example/money/MoneyApplicationTests.java
@@ -1,9 +1,16 @@
 package com.example.money;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb", // use in-memory DB
+		"spring.jpa.hibernate.ddl-auto=none"
+})
 class MoneyApplicationTests {
 
 	@Test
