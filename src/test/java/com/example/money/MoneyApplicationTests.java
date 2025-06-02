94afbdd924a97599e10177ab6f6527aaaccbfb4a
fivaz <s.a.fivaz@gmail.com>
2025-06-02 19:55:17 +0200

    remove db test 2

--- src/test/java/com/example/money/MoneyApplicationTests.java
+++ src/test/java/com/example/money/MoneyApplicationTests.java
@@ -6,11 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(properties = {
-		"spring.datasource.url=jdbc:h2:mem:testdb", // use in-memory DB
-		"spring.jpa.hibernate.ddl-auto=none"
-})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class MoneyApplicationTests {
 
 	@Test
