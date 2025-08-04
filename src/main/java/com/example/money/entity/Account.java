86270b2a15e5d58d305f638b9383241c23654c03
fivaz <31249205+fivaz@users.noreply.github.com>
2025-08-05 01:36:20 +0200

    add account table

---
+++ src/main/java/com/example/money/entity/Account.java
@@ -0,0 +1,42 @@
+package com.example.money.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    private String name;
+
+    private int sortOrder;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private String icon;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("isDeleted")
+    private boolean isDeleted = false;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
